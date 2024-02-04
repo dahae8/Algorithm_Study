9b82ed8c458f59cfa10a861f23806d0196b230f6
Choijinho00 <wlsgh7726@gmail.com>
2024-02-04 15:28:22 +0900

    [BOJ]_2445_별 찍기 - 8 / 3M

---
+++ 02_01/jinho/[BOJ]_2445_별 찍기 - 8.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner inputScanner = new Scanner(System.in);
+        int N = inputScanner.nextInt();
+
+        // 위
+        for (int i = 1; i <= N; i++) {
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            // 공백
+            for (int j = 1; j <= N - i; j++) {
+                System.out.print("  ");
+            }
+
+            for (int m = 1; m <= i; m++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // 아래
+        for (int i = N - 1; i > 0; i--) {
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+
+            // 공백
+            for (int j = 1; j <= N - i; j++) {
+                System.out.print("  ");
+            }
+            for (int m = 1; m <= i; m++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        inputScanner.close();
+    }
+}
