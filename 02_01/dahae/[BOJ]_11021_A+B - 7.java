564e2bcdcbf7e32b52d49ce4e2c0ad5d9636ad8f
dahae8 <dahae8@naver.com>
2024-02-04 14:53:50 +0900

    import java.io.*; / 3M

---
+++ 02_01/dahae/[BOJ]_11021_A+B - 7.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+
+    StringTokenizer st;
+
+    int t = Integer.parseInt(bf.readLine());
+
+    for (int i = 1; i <= t; i++) {
+      sb.append("Case #").append(i).append(": ");
+      st = new StringTokenizer(bf.readLine());
+
+      int a, b;
+      a = Integer.parseInt(st.nextToken());
+      b = Integer.parseInt(st.nextToken());
+      sb.append(a + b).append("\n");
+    }
+    System.out.println(sb);
+  }
+}
