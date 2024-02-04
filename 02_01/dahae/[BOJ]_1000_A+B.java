8b1a9fbcac120f5065adf8467f0681bf6c8398d2
dahae8 <dahae8@naver.com>
2024-02-04 13:05:59 +0900

    [BOJ] [BOJ]_1000_A+B / 1분

---
+++ 02_01/dahae/[BOJ]_1000_A+B.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] argc) {
+    int a, b;
+    Scanner sc = new Scanner(System.in);
+    a = sc.nextInt();
+    b = sc.nextInt();
+
+    System.out.println(a + b);
+  }
+}
