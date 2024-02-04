9fec3e8da20caf236b8597d9f5e5c521847fe9fe
dahae8 <dahae8@naver.com>
2024-02-05 06:43:13 +0900

    [BOJ]_2445_별 찍기 - 8/3m

---
+++ 02_01/dahae/[BOJ]_2445_별 찍기 - 8.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(bf.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=1;i<=t;i++){
+            for(int j=1;j<=i;j++){
+                sb.append("*");
+            }
+            for(int j=1;j<=(t-i)*2;j++){
+                sb.append(" ");
+            }
+            for(int j=1;j<=i;j++){
+                sb.append("*");
+            }
+
+            sb.append("\n");
+        }
+        for(int i=1;i<t;i++){
+            for(int j=1;j<=t-i;j++){
+                sb.append("*");
+            }
+            for(int j=1;j<=i*2;j++){
+                sb.append(" ");
+            }
+            for(int j=1;j<=t-i;j++){
+                sb.append("*");
+            }
+
+            if(t==i)break;
+            sb.append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
