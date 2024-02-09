a12162718ae468a44bd31c285bda5dc1e769b4d2
dahae8 <dahae8@naver.com>
2024-02-10 03:26:54 +0900

    [BOJ]_15688_수 정렬하기 5

--- 02_09/dahae/[BOJ]_15688_수 정렬하기 5.java
+++ 02_09/dahae/[BOJ]_15688_수 정렬하기 5.java
@@ -1,4 +1,24 @@
-package 02_09.dahae;
+import java.io.*;
+import java.util.*;
 
-public class [BOJ]_15688_수 정렬하기 5 {
-  
+public class Main {
+  public static void main(String args[]) throws IOException {
+    BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(bf.readLine());
+
+    int arr[] = new int[n];
+
+    for (int i = 0; i < n; i++) {
+      arr[i] = Integer.parseInt(bf.readLine());
+    }
+
+    Arrays.sort(arr);
+    StringBuilder sb = new StringBuilder();
+    for (int i : arr) {
+      sb.append(i).append("\n");
+    }
+    System.out.print(sb);
+
+  }
+}
