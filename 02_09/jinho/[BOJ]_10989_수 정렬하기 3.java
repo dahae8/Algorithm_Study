5cffd33e3ac4d9aa8a4b633ecd5a95e11ef45291
Choijinho00 <wlsgh7726@gmail.com>
2024-02-14 08:25:44 +0900

    [BOJ]_10989_수 정렬하기 3

---
+++ 02_09/jinho/[BOJ]_10989_수 정렬하기 3.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(bf.readLine());
+
+        int array[] = new int[N];
+        for (int i = 0; i < N; i++) {
+            array[i] = Integer.parseInt(bf.readLine());
+        }
+        Arrays.sort(array); // 오름차순정렬
+
+
+        for (int i : array) {
+            sb.append(i).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
