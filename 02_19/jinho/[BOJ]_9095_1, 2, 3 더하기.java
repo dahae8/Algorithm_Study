edcb003a87d92ea4c57f68127090f846ae3f1ab2
Choijinho00 <wlsgh7726@gmail.com>
2024-02-17 22:14:27 +0900

    [BOJ]_9095_1, 2, 3 더하기

---
+++ 02_19/jinho/[BOJ]_9095_1, 2, 3 더하기.java
@@ -0,0 +1,28 @@
++import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            int N = Integer.parseInt(br.readLine());
+            int result = Count(N);
+            System.out.println(result);
+        }
+    }
+
+    public static int Count(int N) {
+        if (N == 1) {
+            return 1;
+        } else if (N == 2) {
+            return 2;
+        } else if (N == 3) {
+            return 4;
+        }
+
+        return Count(N - 1) + Count(N - 2) + Count(N - 3);
+    }
+}
