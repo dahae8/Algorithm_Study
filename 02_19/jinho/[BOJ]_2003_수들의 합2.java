172bf545c9547c81441c335089b1841467949d50
Choijinho00 <wlsgh7726@gmail.com>
2024-02-17 21:08:46 +0900

    [BOJ]_2003_수들의 합2

---
+++ 02_19/jinho/[BOJ]_2003_수들의 합2.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            int input = Integer.parseInt(st.nextToken());
+            arr[i] = input;
+        }
+
+        int cnt = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int sum = 0;
+            for (int j = i; j < arr.length; j++) {
+                sum = sum + arr[j];
+                if (sum == M) {
+                    cnt++;
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
+}
