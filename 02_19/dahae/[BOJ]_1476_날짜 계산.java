5fbdf849ba51bf3fb8a3b56b2841a0e1607b31d5
dahae8 <dahae8@naver.com>
2024-02-22 04:28:59 +0900

    [BOJ]_1476_날짜 계산

--- 02_19/dahae/[BOJ]_1476_날짜 계산.java
+++ 02_19/dahae/[BOJ]_1476_날짜 계산.java
@@ -1,5 +1,41 @@
-package 02_19.dahae;
+import java.io.*;
+import java.util.*;
 
-public class [BOJ]_1476_날짜 계산 {
-  
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    int e = Integer.parseInt(st.nextToken());
+    int s = Integer.parseInt(st.nextToken());
+    int m = Integer.parseInt(st.nextToken());
+    int el = 15;
+    int sl = 28;
+    int ml = 19;
+
+    int ex = 0;
+    int sx = 0;
+    int mx = 0;
+
+    int r = s;
+
+    while (true) {
+      if ((r - e) % el != 0) {
+        r += sl;
+        continue;
+      }
+      if ((r - s) % sl != 0) {
+        r += sl;
+        continue;
+      }
+      if ((r - m) % ml != 0) {
+        r += sl;
+        continue;
+      }
+      break;
+    }
+    System.out.println(r);
+  }
+
 }
