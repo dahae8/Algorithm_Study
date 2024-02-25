1254063ad92dea372305d557bc983ffc0ca27dff
dahae8 <dahae8@naver.com>
2024-02-26 03:30:55 +0900

    [BOJ]_10815_숫자 카드

--- 02_21/dahae/[BOJ]_10815_숫자 카드.java
+++ 02_21/dahae/[BOJ]_10815_숫자 카드.java
@@ -1,32 +1,32 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
+public class Main {
 
+  public static void main(String args[]) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String args[])throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
 
-        int n = Integer.parseInt(br.readLine());
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        HashSet<Integer> hs = new HashSet<>();
-        for(int i =0;i<n;i++){
-            hs.add(Integer.parseInt(st.nextToken()));
-        }
-
-        int m = Integer.parseInt(br.readLine());
-
-        st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
-        for(int i =0;i<m;i++){
-            int c = Integer.parseInt(st.nextToken());
-            if(hs.contains(c))sb.append(1);
-            else sb.append(0);
-            sb.append(" ");
-        }
-
-        System.out.println(sb);
-
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    HashSet<Integer> hs = new HashSet<>();
+    for (int i = 0; i < n; i++) {
+      hs.add(Integer.parseInt(st.nextToken()));
     }
+
+    int m = Integer.parseInt(br.readLine());
+
+    st = new StringTokenizer(br.readLine());
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < m; i++) {
+      int c = Integer.parseInt(st.nextToken());
+      if (hs.contains(c))
+        sb.append(1);
+      else
+        sb.append(0);
+      sb.append(" ");
+    }
+
+    System.out.println(sb);
+  }
 }
