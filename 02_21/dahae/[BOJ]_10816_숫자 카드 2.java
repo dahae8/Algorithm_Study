95c1c76fa06c7a0a667bc8146407b2f014fb2d25
dahae8 <dahae8@naver.com>
2024-02-26 04:17:36 +0900

    [BOJ]_1654_랜선 자르기

--- 02_21/dahae/[BOJ]_10816_숫자 카드 2.java
+++ 02_21/dahae/[BOJ]_10816_숫자 카드 2.java
@@ -1,5 +1,30 @@
-package 02_21.dahae;
+import java.io.*;
+import java.util.*;
 
-public class [BOJ]_10816_숫자 카드 2 {
-  
+public class Main {
+
+  public static void main(String args[]) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int arr[] = new int[20000001];
+    for (int i = 0; i < n; i++) {
+      arr[Integer.parseInt(st.nextToken()) + 10000000]++;
+    }
+
+    int m = Integer.parseInt(br.readLine());
+
+    st = new StringTokenizer(br.readLine());
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < m; i++) {
+      int c = Integer.parseInt(st.nextToken());
+      sb.append(arr[c + 10000000]);
+      sb.append(" ");
+    }
+
+    System.out.println(sb);
+
+  }
 }
