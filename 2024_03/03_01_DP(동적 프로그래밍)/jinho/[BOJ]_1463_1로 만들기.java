74ce17f58f0c5b1e49869a57f2387163ec36faeb
Choijinho00 <wlsgh7726@gmail.com>
2024-03-01 08:55:50 +0900

    [BOJ]_1463_1로 만들기

--- 2024_03/03_01_DP(동적 프로그래밍)/jinho/[BOJ]_1463_1로 만들기.java
+++ 2024_03/03_01_DP(동적 프로그래밍)/jinho/[BOJ]_1463_1로 만들기.java
@@ -10,16 +10,22 @@ public class Main {
 
         int[] dp = new int[N + 1];
 
-        for (int i = 2; i <= N; i++) {
-            dp[i] = dp[i - 1] + 1;  // 1 뺀 경우
+        for (int i = 2; i < N + 1; i++) {
 
-            if (i % 2 == 0) {
-                dp[i] = Math.min(dp[i], dp[i / 2] + 1);  // 2 나누는 경우
+            if (i % 3 == 0 && i % 2 == 0) {
+                dp[i] = Math.min(dp[i / 3], dp[i / 2]) + 1;
+                if (dp[i] - 1 > dp[i - 1]) dp[i] = dp[i - 1] + 1;
+
+            } else if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i / 3], dp[i - 1]) + 1;
+
+            } else if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i / 2], dp[i - 1]) + 1;
+
+            } else {
+                dp[i] = dp[i - 1] + 1;
             }
 
-            if (i % 3 == 0) {
-                dp[i] = Math.min(dp[i], dp[i / 3] + 1);  // 3 나누는 경우
-            }
         }
 
         System.out.println(dp[N]);
